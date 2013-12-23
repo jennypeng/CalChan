32e9dc54d030d039ae7a845354d24c07621444d6
Dylan Chun <d.chunwong@gmail.com>
2013-12-22 21:45:20 -0800

    now it connects to db

--- expressHello/server.js
+++ expressHello/server.js
@@ -3,6 +3,7 @@ var app = express();
 var mdb = require('mongodb');
 var db;
 var collection;
+boards = ['a', 'b', 'c', 'd'];
 mdb.connect("mongodb://Hello:World@dharma.mongohq.com:10063/Expressr", 
             function(err, base){
                 if (err){return console.dir(err);}
@@ -16,26 +17,27 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname+'/public'));
 app.use(express.bodyParser());
 
-var content = {};
-content.posts = {};
-content.title = "Home";
-content.boards = ['a', 'b', 'c', 'd'];
-
 app.get('/', function(req, res){
-    console.log(content.posts);
-    res.render('index', content);
+    getPosts(function(content){
+        res.render('index', {"title":"Home", "boards":boards, "posts": content});
+    });
 });
 
 app.post('/post-entry', function(req, res){
-    content.title = "pls";
-    name = req.body.post.user;
-    body = req.body.post.content;
-    collection.insert({name: name, body: body}, {w:1}, function(err, result){
+    collection.insert({name: req.body.post.user, body: req.body.post.content}, {w:1}, function(err, result){
         if(err){console.log(err); return;}
-        collection.findAll
+        console.log("post success");
     });
-    content.posts[name] = body;
     res.redirect('/');})
 
 app.listen(3000);
 console.log("listening on port 3000");
+
+function getPosts(callback){
+    collection.find({}).toArray(function(err, docs){
+        if(err){console.log(err); return;}
+        if(docs.length > 0){
+            callback(docs);
+        }
+    })
+}
